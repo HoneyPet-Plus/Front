c1548852454166130a1653efb7368a24454fffb1
danimidi <daniela.mirama02@gmail.com>
2021-11-07 23:29:12 -0500

    iniciando en react

--- rfront/src/index.js
+++ rfront/src/index.js
@@ -3,15 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Proveedores from './Components/ListaProveedores/Proveedores';
+import Home from './Components/Home/Home';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//<App />
 reportWebVitals();
