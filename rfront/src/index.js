0c17470d803823befa5971e1e6ea2546540933fa
Ivan Campos <ivanm.camposlo@gmail.com>
2021-11-22 23:17:13 -0500

    se instalo react bootstrap

--- rfront/src/index.js
+++ rfront/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
-import PublicarNegocio from './Components/PublicarNegocio/PublicarNegocio';
+// import PublicarNegocio from './Components/PublicarNegocio/PublicarNegocio';
 
 
 
